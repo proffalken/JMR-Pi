8255e3fbb33abf692f46067968b069e7f67f7ee6
Matthew Macdonald-Wallace <mmw@greenandsecure.co.uk>
2012-11-12 23:15:37 +0000

    First release - needs testing

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ JMR-Pi
 
 This repo contains scripts to configure JMRI on a Raspberry PI for use in the computer control of layouts.
 
-To get the code, log onto your R-PI and run the following commands:
+To get the code, log onto your R-PI, start a terminal if you do not have one already and run the following commands:
 
 ```bash
 sudo apt-get install git
@@ -18,5 +18,8 @@ This will:
   * Checkout this repository
   * Change to the checked-out code
   * Run the installation script
+  * Create a dedicated JMRI user
+  * Start a remote desktop server
+  * Launch JMRI
 
 
--- setup.sh
+++ setup.sh
@@ -14,7 +14,32 @@ echo "Unpacking the source into /opt"
 cd /opt
 tar -zxf $WORKING_DIR/jmri_downloads/$JMRI_VERSION.tgz
 # installing the correct java txrx library:
-apt-get -y install openjdk-7-jre librxtx-java
+apt-get -y install openjdk-7-jre librxtx-java tightvncserver
 cd /opt/JMRI/lib/linux/armv5
 mv librxtxSerial.so librxtxSerial.so.jmri
 ln -s /usr/lib/jni/librxtxSerial.so
+
+# create the jmri user that we will run as:
+useradd -m jmri
+
+# copy the files to the correct location:
+cp $WORKING_DIR/scripts/init.d/vncserver /etc/init.d/vncserver
+chmod +x /etc/init.d/vncserver
+mkdir -p /home/jmri/.config/lxsession/LXDE
+echo '@/opt/JMRI/PanelPro' >> /home/jmri/.config/lxsession/LXDE/autostart
+mkdir /home/jmri/.vnc
+cp $WORKING_DIR/scripts/passwd /home/jmri/.vnc/passwd
+chown -Rf jmri: /home/jmri
+# start the services:
+/etc/init.d/vncserver start
+
+# get the current ip address
+if [ -e /sys/class/net/wlan0 ]; then
+  export INTERFACE="wlan0"
+elif [ -e /sys/class/net/eth0 ]; then
+  export INTERFACE="eth0"
+fi
+IPADDRESS="$(ifconfig $INTERFACE | sed -n '/^[A-Za-z0-9]/ {N;/dr:/{;s/.*dr://;s/ .*//;p;}}')"
+
+# echo the details:
+echo -e "Your JMRI server is ready...\n============\n\nPlease connect to $IPADDRESSi:5901 with a VNC client to configure JMRI.\n\nPlease note that JMRI will take several minutes to start the first time it is run."
